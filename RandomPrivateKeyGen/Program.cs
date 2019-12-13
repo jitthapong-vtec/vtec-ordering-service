72c50d18c6ef91b63ce00f51ef2a53d976e0f75a
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2019-12-13 17:46:17 +0700

    update

---
+++ RandomPrivateKeyGen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RandomPrivateKeyGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rng = System.Security.Cryptography.RandomNumberGenerator.Create();
+            var bytes = new byte[256 / 8];
+            rng.GetBytes(bytes);
+            var privateKey = Convert.ToBase64String(bytes);
+            Console.WriteLine(privateKey);
+        }
+    }
+}
