3dcfd79c5af2c8f41e1d8e7e6b1759e3c63505eb
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2023-10-09 15:21:06 +0700

    lock login

--- VerticalTec.POS.Service.Ordering.Owin/Controllers/StaffController.cs
+++ VerticalTec.POS.Service.Ordering.Owin/Controllers/StaffController.cs
@@ -13,6 +13,8 @@ namespace VerticalTec.POS.Service.Ordering.Owin.Controllers
 {
     public class StaffController : ApiController
     {
+        public static readonly object Owner = new object();
+
         IDatabase _database;
         VtecPOSRepo _posRepo;
 
@@ -44,109 +46,112 @@ namespace VerticalTec.POS.Service.Ordering.Owin.Controllers
 
         [HttpPost]
         [Route("v1/staffs/identify")]
-        public async Task<IHttpActionResult> IdentifyStaff(string staffCode = "", string password = "", int shopId = 0, int terminalId = 0)
+        public IHttpActionResult IdentifyStaff(string staffCode = "", string password = "", int shopId = 0, int terminalId = 0)
         {
-            var result = new HttpActionResult<object>(Request);
-            using (IDbConnection conn = await _database.ConnectAsync())
+            lock (Owner)
             {
-                var dtStaff = await _posRepo.GetStaffAsync(conn, staffCode, password);
-                var dtStaffPermission = await _posRepo.GetStaffPermissionAsync(conn);
-
-                if (dtStaff.Rows.Count > 0)
+                var result = new HttpActionResult<object>(Request);
+                using (IDbConnection conn = _database.ConnectAsync().Result)
                 {
-                    var staffId = dtStaff.Rows[0].GetValue<int>("StaffID");
-                    var allowAccess = true;
-                    if (shopId > 0)
-                        allowAccess = await _posRepo.CheckStaffAccessShop(conn, staffId, shopId);
+                    var dtStaff = _posRepo.GetStaffAsync(conn, staffCode, password).Result;
+                    var dtStaffPermission = _posRepo.GetStaffPermissionAsync(conn).Result;
 
-                    if (allowAccess)
+                    if (dtStaff.Rows.Count > 0)
                     {
-                        var staff = (from row in dtStaff.AsEnumerable()
-                                     select new
-                                     {
-                                         StaffID = row.GetValue<int>("StaffID"),
-                                         StaffRoleID = row.GetValue<int>("StaffRoleID"),
-                                         StaffFirstName = row.GetValue<string>("StaffFirstName"),
-                                         StaffLastName = row.GetValue<string>("StaffLastName"),
-                                         LangID = row.GetValue<int>("LangID"),
-                                         Permissions = (from permission in dtStaffPermission.Select($"StaffRoleID={row.GetValue<int>("StaffRoleID")}")
-                                                        select new
-                                                        {
-                                                            PermissionItemID = permission.GetValue<int>("PermissionItemID")
-                                                        }).ToList()
-                                     }).FirstOrDefault();
+                        var staffId = dtStaff.Rows[0].GetValue<int>("StaffID");
+                        var allowAccess = true;
+                        if (shopId > 0)
+                            allowAccess = _posRepo.CheckStaffAccessShop(conn, staffId, shopId).Result;
 
-
-                        var dtProperty = await _posRepo.GetProgramPropertyAsync(conn, 1097);
-                        var isSingleLogin = dtProperty.AsEnumerable().Select(r => r.GetValue<int>("PropertyValue") == 1).FirstOrDefault();
-                        if (isSingleLogin)
+                        if (allowAccess)
                         {
-                            var dtComputerAccess = new DataTable();
-                            var cmd = new MySqlCommand("select a.*, b.ComputerName from computeraccess a" +
-                                " join ComputerName b " +
-                                " on a.ComputerID=b.ComputerID" +
-                                " where a.LastLoginStaffID=@staffId and a.ComputerID != @terminalId", (MySqlConnection)conn);
-                            cmd.Parameters.Clear();
-                            cmd.Parameters.AddRange(new MySqlParameter[]
+                            var staff = (from row in dtStaff.AsEnumerable()
+                                         select new
+                                         {
+                                             StaffID = row.GetValue<int>("StaffID"),
+                                             StaffRoleID = row.GetValue<int>("StaffRoleID"),
+                                             StaffFirstName = row.GetValue<string>("StaffFirstName"),
+                                             StaffLastName = row.GetValue<string>("StaffLastName"),
+                                             LangID = row.GetValue<int>("LangID"),
+                                             Permissions = (from permission in dtStaffPermission.Select($"StaffRoleID={row.GetValue<int>("StaffRoleID")}")
+                                                            select new
+                                                            {
+                                                                PermissionItemID = permission.GetValue<int>("PermissionItemID")
+                                                            }).ToList()
+                                         }).FirstOrDefault();
+
+
+                            var dtProperty = _posRepo.GetProgramPropertyAsync(conn, 1097).Result;
+                            var isSingleLogin = dtProperty.AsEnumerable().Select(r => r.GetValue<int>("PropertyValue") == 1).FirstOrDefault();
+                            if (isSingleLogin)
                             {
-                                new MySqlParameter("@staffId", staff.StaffID),
-                                new MySqlParameter("@terminalId", terminalId)
-                            });
-                            using (var reader = await cmd.ExecuteReaderAsync())
-                            {
-                                dtComputerAccess.Load(reader);
-                            }
-                            if (dtComputerAccess.Rows.Count > 0)
-                            {
-                                var row = dtComputerAccess.AsEnumerable().First();
-                                result.StatusCode = HttpStatusCode.OK;
-                                result.Body = new
-                                {
-                                    Code = "ACCESS_ANOTHER_COMPUTER",
-                                    ComputerName = row["ComputerName"]
-                                };
-                            }
-                            else
-                            {
-                                cmd = new MySqlCommand("delete from computeraccess where ShopID=@shopId and ComputerID=@terminalId", (MySqlConnection)conn);
+                                var dtComputerAccess = new DataTable();
+                                var cmd = new MySqlCommand("select a.*, b.ComputerName from computeraccess a" +
+                                    " join ComputerName b " +
+                                    " on a.ComputerID=b.ComputerID" +
+                                    " where a.LastLoginStaffID=@staffId and a.ComputerID != @terminalId", (MySqlConnection)conn);
                                 cmd.Parameters.Clear();
                                 cmd.Parameters.AddRange(new MySqlParameter[]
                                 {
+                                new MySqlParameter("@staffId", staff.StaffID),
+                                new MySqlParameter("@terminalId", terminalId)
+                                });
+                                using (var reader = cmd.ExecuteReader())
+                                {
+                                    dtComputerAccess.Load(reader);
+                                }
+                                if (dtComputerAccess.Rows.Count > 0)
+                                {
+                                    var row = dtComputerAccess.AsEnumerable().First();
+                                    result.StatusCode = HttpStatusCode.OK;
+                                    result.Body = new
+                                    {
+                                        Code = "ACCESS_ANOTHER_COMPUTER",
+                                        ComputerName = row["ComputerName"]
+                                    };
+                                }
+                                else
+                                {
+                                    cmd = new MySqlCommand("delete from computeraccess where ShopID=@shopId and ComputerID=@terminalId", (MySqlConnection)conn);
+                                    cmd.Parameters.Clear();
+                                    cmd.Parameters.AddRange(new MySqlParameter[]
+                                    {
                                     new MySqlParameter("@shopId", shopId),
                                     new MySqlParameter("@terminalId", terminalId)
-                                });
-                                await cmd.ExecuteNonQueryAsync();
+                                    });
+                                    cmd.ExecuteNonQuery();
 
-                                cmd = new MySqlCommand("insert into computeraccess(ComputerID,ShopID,StockToInvID,LastLoginStaffID) values (@terminalId,@shopId,@shopId,@staffId)", (MySqlConnection)conn);
-                                cmd.Parameters.AddRange(new MySqlParameter[]
-                                {
+                                    cmd = new MySqlCommand("insert into computeraccess(ComputerID,ShopID,StockToInvID,LastLoginStaffID) values (@terminalId,@shopId,@shopId,@staffId)", (MySqlConnection)conn);
+                                    cmd.Parameters.AddRange(new MySqlParameter[]
+                                    {
                                     new MySqlParameter("@shopId", shopId),
                                     new MySqlParameter("@terminalId", terminalId),
                                     new MySqlParameter("@staffId", staff.StaffID)
-                                });
-                                await cmd.ExecuteNonQueryAsync();
+                                    });
+                                    cmd.ExecuteNonQuery();
 
+                                    result.StatusCode = HttpStatusCode.OK;
+                                    result.Body = staff;
+                                }
+                            }
+                            else
+                            {
                                 result.StatusCode = HttpStatusCode.OK;
                                 result.Body = staff;
                             }
                         }
                         else
                         {
-                            result.StatusCode = HttpStatusCode.OK;
-                            result.Body = staff;
+                            result.StatusCode = HttpStatusCode.Unauthorized;
                         }
                     }
                     else
                     {
-                        result.StatusCode = HttpStatusCode.Unauthorized;
+                        result.StatusCode = HttpStatusCode.NotFound;
                     }
                 }
-                else
-                {
-                    result.StatusCode = HttpStatusCode.NotFound;
-                }
+                return result;
             }
-            return result;
         }
     }
 }
