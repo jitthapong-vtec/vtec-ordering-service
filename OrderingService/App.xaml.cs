06db763db109490454cdd66568f98212e9dd032c
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2024-03-28 13:56:39 +0700

    add orderingservice app

---
+++ OrderingService/App.xaml.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Owin.Hosting;
+using OrderingService.Properties;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using Form = System.Windows.Forms;
+
+namespace OrderingService
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IDisposable _host;
+
+        private Form.NotifyIcon _notifyIcon;
+        private Form.ToolStripMenuItem _menuSetting;
+        private Form.ToolStripMenuItem _menuExit;
+
+        public App()
+        {
+            InitToolStripMenu();
+        }
+
+        private void InitToolStripMenu()
+        {
+            _notifyIcon = new Form.NotifyIcon()
+            {
+                ContextMenuStrip = new Form.ContextMenuStrip(),
+                Text = "vTec Ordering Service",
+                Visible = true
+            };
+
+            _notifyIcon.Click += _notifyIcon_Click;
+
+            try
+            {
+                var res = GetResourceStream(new Uri("pack://application:,,,/OrderingService;component/Resources/Icon/Disk.ico"));
+                _notifyIcon.Icon = new System.Drawing.Icon(res.Stream);
+                _notifyIcon.BalloonTipIcon = Form.ToolTipIcon.Info;
+            }
+            catch { }
+
+            System.Drawing.Image settingImg = null;
+            try
+            {
+                var res = GetResourceStream(new Uri("pack://application:,,,/OrderingService;component/Resources/Icon/Settings-36.png"));
+                settingImg = new System.Drawing.Bitmap(res.Stream);
+            }
+            catch { }
+
+            System.Drawing.Image exitImg = null;
+            try
+            {
+                var res = GetResourceStream(new Uri("pack://application:,,,/OrderingService;component/Resources/Icon/Shutdown-40.png"));
+                exitImg = new System.Drawing.Bitmap(res.Stream);
+            }
+            catch { }
+
+            _menuSetting = new Form.ToolStripMenuItem("Settings", settingImg, ShowSettingWindow, "SettingMenu");
+            _menuExit = new Form.ToolStripMenuItem("Exit", exitImg, ExitApp, "ExitMenu");
+
+            var menus = new[] { _menuSetting, _menuExit };
+            _notifyIcon.ContextMenuStrip.Items.AddRange(menus);
+        }
+
+        private void ExitApp(object sender, EventArgs e)
+        {
+            Shutdown();
+        }
+
+        private void ShowSettingWindow(object sender, EventArgs e)
+        {
+            var settingWindow = new SettingWindow();
+            var result = settingWindow.ShowDialog();
+            if (result == true)
+            {
+                RunOrderingApi();
+            }
+        }
+
+        private void _notifyIcon_Click(object sender, EventArgs e)
+        {
+            var args = e as Form.MouseEventArgs;
+            if (args?.Button == Form.MouseButtons.Left)
+            {
+                MethodInfo mi = typeof(Form.NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+                mi?.Invoke(_notifyIcon, null);
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            RunOrderingApi();
+
+            _notifyIcon.ShowBalloonTip(5000, "vTec Ordering Service", "vTec ordering service is running", Form.ToolTipIcon.Info);
+        }
+
+        private void RunOrderingApi()
+        {
+            var dbServer = Settings.Default.DBServer;
+            var dbName = Settings.Default.DBName;
+            var apiPort = Settings.Default.APIPort;
+            var rcAgentPath = Settings.Default.RCAgentPath;
+
+            //var baseAddress = $"http://+:{apiPort}/";
+            var baseAddress = $"http://127.0.0.1:{apiPort}";
+            try
+            {
+                var hangfireConStr = Path.GetDirectoryName(Uri.UnescapeDataString(new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).AbsolutePath)) + "\\hangfire.db";
+
+                _host?.Dispose();
+                _host = WebApp.Start(baseAddress,
+                    appBuilder => new VerticalTec.POS.Service.Ordering.Owin.Startup(dbServer, dbName, hangfireConStr, rcAgentPath).Configuration(appBuilder));
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host?.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ OrderingService/SettingWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using OrderingService.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OrderingService
+{
+    /// <summary>
+    /// Interaction logic for SettingWindow.xaml
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        public SettingWindow()
+        {
+            InitializeComponent();
+
+            txtDbServer.Text = Settings.Default.DBServer;
+            txtDbName.Text = Settings.Default.DBName;
+            txtApiPort.Text = Settings.Default.APIPort;
+            txtRCAgentPath.Text = Settings.Default.RCAgentPath;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.DBServer = txtDbServer.Text;
+            Settings.Default.DBName = txtDbName.Text;
+            Settings.Default.APIPort = txtApiPort.Text;
+            Settings.Default.Save();
+
+            DialogResult = true;
+            Close();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.ShowDialog();
+                txtRCAgentPath.Text = dialog.SelectedPath;
+            }
+        }
+    }
+}
