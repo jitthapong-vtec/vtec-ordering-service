f7f30160416d2aaf025c8abcae5697538d305650
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2024-06-13 11:56:30 +0700

    update inventory controller

--- VerticalTec.POS.Service.Ordering.Owin/controllers/InventoryController.cs
+++ VerticalTec.POS.Service.Ordering.Owin/controllers/InventoryController.cs
@@ -18,7 +18,7 @@ using vtecPOS.POSControl;
 
 namespace VerticalTec.POS.Service.Ordering.Owin.Controllers
 {
-    [RoutePrefix("v1/inventory")]
+    [RoutePrefix("inventory")]
 
     public class InventoryController : ApiController
     {
@@ -34,42 +34,128 @@ namespace VerticalTec.POS.Service.Ordering.Owin.Controllers
             _inventModule = new InventModule();
         }
 
+        [HttpPost]
+        [Route("hht/createdocument")]
+        public async Task<IHttpActionResult> CreateDocument(InventObject.DocumentData documentData, int staffId = 2, int langId = 1)
+        {
+            try
+            {
+                using (var conn = (MySqlConnection)await _database.ConnectAsync())
+                {
+                    var docParam = new InventObject.DocParam()
+                    {
+                        StaffID = staffId,
+                        DocumentDate = documentData.DocHeader.DocumentDate,
+                        DocumentTypeID = documentData.DocHeader.DocumentTypeID,
+                        LangID = langId
+                    };
+
+                    var docData = await Task.Run(() =>
+                    {
+                        var respText = "";
+                        var resultDocData = new InventObject.DocumentData();
+                        using (var _ = new InvariantCultureScope())
+                        {
+                            if (_inventModule.DocumentProcess(ref respText, ref resultDocData, documentData, docParam, conn) == false)
+                                throw new Exception($"DocumentProcess: {respText}");
+
+                            if (documentData?.DocDetail?.Any() == true)
+                            {
+                                documentData.DocDetail.ForEach(d =>
+                                {
+                                    var docDetail = new InventObject.DocDetail();
+                                    if (_inventModule.DocDetailObj(ref respText, ref docDetail, d.DocDetailID, documentData.DocHeader.DocumentKey, d.MaterialID, "front", conn) == false)
+                                        throw new Exception($"DocDetailObj: {respText}");
+
+                                    var defaultUnit = docDetail.UnitList?.Where(u => u.UnitLargeID == docDetail.UnitLargeID).FirstOrDefault();
+                                    if (defaultUnit == null)
+                                        throw new Exception($"Not found unit for material {docDetail.MaterialID}");
+
+                                    docDetail.UnitName = defaultUnit.UnitName;
+                                    docDetail.UnitSmallID = defaultUnit.UnitSmallID;
+                                    docDetail.UnitLargeID = defaultUnit.UnitLargeID;
+                                    docDetail.UnitRatio = defaultUnit.UnitRatio.ToString();
+                                    docDetail.UnitLargeRatio = defaultUnit.UnitLargeRatio.ToString();
+
+                                    d = docDetail;
+                                });
+
+                                if (_inventModule.DocDetail_Add(ref respText, ref resultDocData, documentData.DocDetail, documentData.DocHeader, conn) == false)
+                                    throw new Exception($"DocDetail_Add: {respText}");
+                            }
+                        }
+                        return resultDocData;
+                    });
+
+                    return Ok(new
+                    {
+                        Status = HttpStatusCode.OK,
+                        StatusCode = "200.201",
+                        Data = docData
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    Status = HttpStatusCode.InternalServerError,
+                    StatusCode = "500.500",
+                    Message = ex.Message
+                });
+            }
+        }
+
         [HttpGet]
-        [Route("Materials")]
+        [Route("materials")]
         public async Task<IHttpActionResult> GetMaterials(int documentType = 0)
         {
-            using (var conn = (MySqlConnection)await _database.ConnectAsync())
+            try
             {
-                var docHeader = new InventObject.DocHeader
+                using (var conn = (MySqlConnection)await _database.ConnectAsync())
                 {
-                    ShopID = 0,
-                    DocumentDate = DateTime.Today.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
-                    DueDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture),
-                    DocumentTypeID = documentType,
-                    DocumentNo = "-",
-                    ItemDiscount = "0.00",
-                    BillDiscount = "0.00",
-                    BillDiscPercent = "0",
-                    SubTotalAmt = "0.00",
-                    TotalAmt = "0.00",
-                    GrandTotalAmt = "0.00",
-                    TotalVAT = "0.00",
-                    TotalDiscount = "0.00"
-                };
+                    var docHeader = new InventObject.DocHeader
+                    {
+                        ShopID = 0,
+                        DocumentDate = DateTime.Today.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
+                        DueDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture),
+                        DocumentTypeID = documentType,
+                        DocumentNo = "-",
+                        ItemDiscount = "0.00",
+                        BillDiscount = "0.00",
+                        BillDiscPercent = "0",
+                        SubTotalAmt = "0.00",
+                        TotalAmt = "0.00",
+                        GrandTotalAmt = "0.00",
+                        TotalVAT = "0.00",
+                        TotalDiscount = "0.00"
+                    };
 
-                var materials = await Task.Run(() =>
-                {
-                    var respText = "";
-                    var materialData = new InventObject.MaterialData();
-                    if (_inventModule.Material_Data(ref respText, ref materialData, docHeader, "", "", conn) == false)
-                        throw new Exception(respText);
-                    return materialData;
-                });
+                    var materials = await Task.Run(() =>
+                    {
+                        var respText = "";
+                        var materialData = new InventObject.MaterialData();
+                        if (_inventModule.Material_Data(ref respText, ref materialData, docHeader, "", "", conn) == false)
+                            throw new Exception(respText);
+                        return materialData;
+                    });
+
+                    return Ok(new
+                    {
+                        Status = HttpStatusCode.OK,
+                        StatusCode = "200.200",
+                        Data = materials
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
 
                 return Ok(new
                 {
-                    Status = HttpStatusCode.OK,
-                    Data = materials
+                    Status = HttpStatusCode.InternalServerError,
+                    StatusCode = "500.500",
+                    Message = ex.Message
                 });
             }
         }
