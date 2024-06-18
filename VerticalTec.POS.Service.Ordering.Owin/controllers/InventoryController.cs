3edc1dec16c5e95b4beaa9072822a9b8089041a2
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2024-06-18 12:26:09 +0700

    update inventory

--- VerticalTec.POS.Service.Ordering.Owin/controllers/InventoryController.cs
+++ VerticalTec.POS.Service.Ordering.Owin/controllers/InventoryController.cs
@@ -1,6 +1,7 @@
 ﻿using DevExpress.XtraExport.Helpers;
 using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -32,6 +33,95 @@ namespace VerticalTec.POS.Service.Ordering.Owin.Controllers
             _vtecRepo = vtecRepo;
 
             _inventModule = new InventModule();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IHttpActionResult> LoginAsync(object data)
+        {
+            try
+            {
+                var jObj = JObject.Parse(data.ToString());
+                using (var conn = (MySqlConnection)await _database.ConnectAsync())
+                {
+                    var cmd = new MySqlCommand("select StaffID, StaffRoleID, StaffFirstName, StaffLastName from staffs where Deleted=0 and StaffLogin=@username and StaffPassword=UPPER(SHA1(@password))", conn);
+                    cmd.Parameters.Add(new MySqlParameter("@username", jObj["username"]));
+                    cmd.Parameters.Add(new MySqlParameter("@password", jObj["password"]));
+
+                    var dt = new DataTable();
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        dt.Load(reader);
+                    }
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        return Ok(new
+                        {
+                            Status = HttpStatusCode.OK,
+                            StatusCode = "200.200",
+                            Data = dt.AsEnumerable().Select(r => new
+                            {
+                                StaffID = r["StaffID"],
+                                StaffRoleID = r["StaffRoleID"],
+                                StaffFirstName = r["StaffFirstName"],
+                                SstaffLastName = r["StaffLastName"]
+                            }).FirstOrDefault()
+                        });
+                    }
+                    else
+                    {
+                        return Ok(new
+                        {
+                            Status = HttpStatusCode.NotFound,
+                            StatusCode = "404.404",
+                            Message = "Not found staff information"
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    Status = HttpStatusCode.InternalServerError,
+                    StatusCode = "500.500",
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpGet]
+        [Route("shops")]
+        public async Task<IHttpActionResult> GetShopAsync()
+        {
+            try
+            {
+                using (var conn = (MySqlConnection)await _database.ConnectAsync())
+                {
+                    var cmd = new MySqlCommand("select ShopID,ShopCode,ShopName from shop_data where Deleted=0 and IsInv=1", conn);
+                    var dt = new DataTable();
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        dt.Load(reader);
+                    }
+                    return Ok(new
+                    {
+                        Status = HttpStatusCode.OK,
+                        StatusCode = "200.200",
+                        Data = dt
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    Status = HttpStatusCode.InternalServerError,
+                    StatusCode = "500.500",
+                    Message = ex.Message
+                });
+            }
         }
 
         [HttpPost]
