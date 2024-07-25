2f57a2896af63e5c0a95b5fa7e5caea1823de429
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2024-07-25 16:43:08 +0700

    update

--- VerticalTec.POS.Service.Ordering/VtecOrderingService.cs
+++ VerticalTec.POS.Service.Ordering/VtecOrderingService.cs
@@ -1,18 +1,8 @@
 ﻿using Microsoft.Owin.Hosting;
-using Microsoft.TeamFoundation.Common;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using VerticalTec.POS.Service.Ordering.Owin;
-using VerticalTec.POS.Service.Ordering.ThirdpartyInterface;
-using VerticalTec.POS.Utils;
 
 namespace VerticalTec.POS.Service.Ordering
 {
@@ -21,7 +11,6 @@ namespace VerticalTec.POS.Service.Ordering
         static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 
         IDisposable _server;
-        OrderServiceWorker _orderServiceWorker;
 
         public VtecOrderingService()
         {
@@ -41,23 +30,11 @@ namespace VerticalTec.POS.Service.Ordering
 
             var hangfireConStr = Path.GetDirectoryName(Uri.UnescapeDataString(new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).AbsolutePath)) + "\\hangfire.db";
             _server = WebApp.Start(baseAddress, appBuilder => new Startup(dbServer, dbName, hangfireConStr, apiUser: apiUser, apiPass: apiPassword).Configuration(appBuilder));
-
-            try
-            {
-                _orderServiceWorker = new OrderServiceWorker(dbServer, dbName);
-                Task.Run(() => _orderServiceWorker.InitConnectionAsync());
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex, "OrderServiceWorker InitConnectionAsync");
-            }
-
         }
 
         protected override void OnStop()
         {
             _server?.Dispose();
-            _orderServiceWorker?.Dispose();
         }
     }
 }
